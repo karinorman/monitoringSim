f7e49d956f65ff7fe1f195c0b82b1f830d973a42
karinorman <kari@normanfamily.org>
2022-09-01 15:32:46 +0000

    get rid of old stuff that didn't work

--- stac_data.R
+++ stac_data.R
@@ -26,6 +26,7 @@ stac(stac_path) %>%
 # maybe of interest but not sure? "ghmts", "stressors_qc", "silvis",
 sources <- list("chelsa-clim", "soilgrids", "earthenv_topography",
                 "earthenv_habitat_heterogeneity")#, "ghmts",  "stressors_qc")
+
 source_meta <- purrr::map_dfr(sources, ~get_info_collection(stac_path =
                                                               "http://io.biodiversite-quebec.ca/stac/",
                                                             collections = c(.x),
@@ -34,6 +35,11 @@ source_meta <- purrr::map_dfr(sources, ~get_info_collection(stac_path =
                                 as_tibble(.) %>%
                                 mutate(source = .x))
 
+# or get info for a single source
+info_chelsa_climate<- get_info_collection(stac_path =
+                                            "http://io.biodiversite-quebec.ca/stac/",
+                                          collections = c('chelsa-clim'),
+                                          bbox = NULL)
 
 #get study extent
 load(here::here("data/qc.rda"))
@@ -90,70 +96,4 @@ layers_df <- tibble(
 
 
 pmap(layers_df, get_STAClayer, file_path = here::here("data/env_layers"), bbox = bounds, cube_projection = srs_cube)
-
-
-
-
-
-
-
-
-# reproject to data cube projection
-srs_cube <- "EPSG:6623"
-bbox <- stacatalogue::shp_to_bbox(qc,
-                                  proj_to = srs_cube)
-
-# get potential sample points
-load(here::here("data/qc_cand_points.rda"))
-qc_pts <- stacatalogue::project_coords(qc_cand_points %>%
-                                         dplyr::mutate(lon = sf::st_coordinates(.)[,1],
-                                                       lat = sf::st_coordinates(.)[,2]) %>%
-                                         st_drop_geometry(),
-                                       lon = "lon",
-                                       lat = "lat",
-                                       proj_from = "EPSG:5070",
-                                       proj_to = srs_cube)
-
-#### Exploring data #####
-# let's get a dataframe of datasources at variables
-source_meta <- purrr::map_dfr(sources, ~get_info_collection(stac_path =
-                                                         "http://io.biodiversite-quebec.ca/stac/",
-                                                       collections = c(.x),
-                                                       bbox = NULL) %>%
-                                within(., rm("layers")) %>%
-                                as_tibble(.) %>%
-                                mutate(source = .x))
-
-
-info_chelsa_climate<- get_info_collection(stac_path =
-                                             "http://io.biodiversite-quebec.ca/stac/",
-                                           collections = c('chelsa-clim'),
-                                           bbox = NULL)
-
-# # get cube of all chelsea climate variables
-chelsea_cube <- load_cube(stac_path = stac_path,
-                          collections = c("chelsa-clim"),
-                          bbox = bbox,
-                          limit = 5000,
-                          srs.cube = srs_cube,
-                          variable = info_chelsa_climate$variables,
-                          spatial.res = 2000, # in meters
-                          temporal.res = "P1Y",
-                          aggregation = "mean",
-                          resampling = "near")
-
-# get list of cubes from the sources we want
-cube_list <- purrr::map(sources, ~load_cube(stac_path = stac_path,
-                                           collections = c(.x),
-                                           bbox = bbox,
-                                           limit = 10000,
-                                           srs.cube = srs_cube,
-                                           #variable = info_chelsa_climate$variables,
-                                           #spatial.res = 2000, # in meters
-                                           temporal.res = "P1Y",
-                                           aggregation = "mean",
-                                           resampling = "near"))
-
-test_values <- gdalcubes::extract_geom(cube_list[[1]], sf::st_as_sf(qc_pts[1:10,]))
-
 
